46eef7b5015e135a0e72d22e28b0c668c572047b
Edward <tacasedwardz@gmail.com>
2022-09-13 21:20:27 -0500

    correccion3

--- method_get.py
+++ method_get.py
@@ -12,21 +12,28 @@ def shutdown():
     if not database.is_closed():
         database.close()
 
+
+@app.get("/")
+async def getMain():
+    return ['/race','/qualifying','/circuits','/results','/constructors','/drivers','/lapTimes','/pitStops']
+
 @app.get('/{tabla}')
 async def getData(tabla:str):
-    if tabla == 'race':
-        return races()
-    elif tabla == 'qualifying':
-        return qualif()
-    elif tabla == 'circuits':
-        return circuits()
-    elif tabla == 'results':
-        return results()
-    elif tabla == 'constructors':
-        return constructors()
-    elif tabla == 'drivers':
-        return drivers()
-    elif tabla == 'lapTimes':
-        return lapTimes()
-    elif tabla == 'pitStops':
-        return pitStops()
+
+        if tabla == 'race':
+            return races()
+        elif tabla == 'qualifying':
+            return qualif()
+        elif tabla == 'circuits':
+            return circuits()
+        elif tabla == 'results':
+            return results()
+        elif tabla == 'constructors':
+            return constructors()
+        elif tabla == 'drivers':
+            return drivers()
+        elif tabla == 'lapTimes':
+            return lapTimes()
+        elif tabla == 'pitStops':
+            return pitStops()
+    
